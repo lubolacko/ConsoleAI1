4bdb720c1b0f657166ff98f10ae84cb2157806d6
llack <llack@LL-DESKTOP>
2024-11-06 17:33:56 +0100

    Add project files.

---
+++ ConsoleAI1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.AI;
+
+namespace ChatApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("odpoveď na výzvu What is AI?");
+            await Chat();
+        }
+
+        public static async Task Chat()
+        {
+            var endpoint = new Uri("http://localhost:11434/");
+            var modelId = "llama3.2";
+
+            IChatClient client = new OllamaChatClient(endpoint, modelId: modelId);
+
+            var response = await client.CompleteAsync("What is AI?");
+
+            Console.WriteLine(response.Message);
+        }
+    }
+}
+
